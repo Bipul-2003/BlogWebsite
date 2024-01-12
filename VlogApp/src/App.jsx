26322fc71243f04f2d3a17c04abc2d862f08eda1
Bipul Swain <bipulswain365@gmail.com>
2024-01-12 21:01:57 +0530

    Auth control done.

--- VlogApp/src/App.jsx
+++ VlogApp/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const App = () => {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
-    <div>
-      <div className="bg-slate-600">Hello</div>
+    <div className="flex justify-center items-center h-screen bg-[#1b1b1a] text-cyan-50">
+      <div className="bg-slate-700 p-8 rounded-lg hover:bg-white hover:text-black hover:scale-150 transition ease-in-out">Hello</div>
     </div>
+
   )
 }
 
